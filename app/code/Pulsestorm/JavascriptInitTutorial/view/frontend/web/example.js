39163d59656b1459c5882d4765779eab577d0cc8
aprilwu <1290349271@qq.com>
2019-01-02 12:38:39 +0800

    javascript init scripts

--- app/code/Pulsestorm/JavascriptInitTutorial/view/frontend/web/example.js
+++ app/code/Pulsestorm/JavascriptInitTutorial/view/frontend/web/example.js
@@ -1,4 +1,20 @@
-define([], function(){
-    alert("A simple RequireJS module");
-    return {};
+define([], function () {
+    //var mageJsComponent = function(config)
+    //{
+        //alert("Look in your browser's console");
+        //console.log(config);
+        //alert(config);
+    //};
+
+
+
+    var mageJsComponent = function(config, node)
+    {
+        console.log(config);
+        console.log(node);
+        //alert(config);
+    };
+
+    return mageJsComponent;
+
 });
