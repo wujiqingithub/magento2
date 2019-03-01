a96bc1a5b158787d7deaedcc2309775fd8348820
aprilwu <1290349271@qq.com>
2019-03-01 17:32:52 +0800

    Revert "rm pub/static from git repository"
    This reverts commit 8fa4fee23bc7a07d1ed720cb677b6bdd79b6f551.

--- pub/static/frontend/Magento/luma/en_US/requirejs-config.js
+++ pub/static/frontend/Magento/luma/en_US/requirejs-config.js
@@ -742,20 +742,6 @@ require.config(config);
 })();
 (function() {
 var config = {
-    paths:{
-        "addjs":"Pulsestorm_RequireJsTutorial/js/addjs"
-    },
-    shim:{
-        'addjs':{
-            'deps':['jquery']
-        }
-    }
-};
-
-require.config(config);
-})();
-(function() {
-var config = {
     paths: {
         temandoCheckoutFieldsDefinition: 'Temando_Shipping/js/model/fields-definition',
         temandoDeliveryOptions: 'Temando_Shipping/js/model/delivery-options',
